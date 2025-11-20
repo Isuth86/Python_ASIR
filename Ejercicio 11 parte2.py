9effbe80c571e02a5403be301808a5db55c519dd
Isma86 <isberrio86@gmail.com>
2025-11-20 12:29:50 +0100

    Agregar comentarios descriptivos al archivo de la escalera de asteriscos

---
+++ Ejercicio 11 parte2.py
@@ -0,0 +1,2 @@
+# Ejercicio 11
+# Crea una aplicación que dibuje una escalera de asteriscos. Nosotros le pasamos la altura de la escalera por teclado. Este es un ejemplo si insertaras un 5 de altura:
