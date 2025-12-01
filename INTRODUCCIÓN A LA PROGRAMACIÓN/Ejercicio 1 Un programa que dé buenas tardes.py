d49788b611a12ea2af9dee6af5765f246494cbf2
Isma86 <isberrio86@gmail.com>
2025-12-01 21:14:14 +0100

    *

---
+++ INTRODUCCIÓN A LA PROGRAMACIÓN/Ejercicio 1 Un programa que dé buenas tardes.py
@@ -0,0 +1,8 @@
+# Ejercicio 1 Un programa que dé buenas tardes.
+
+hora = int(input("Por favor, ingrese la hora actual (0-23): "))
+if 13 <= hora < 21:
+    print("¡Buenas tardes!")
+else:
+    print("No es la hora de buenas tardes.")
+# Fin del programa
---
+++ INTRODUCCIÓN A LA PROGRAMACIÓN/Ejercicio 2 Un programa que calcule y muestre el área de un cuadrado de lado igual a 5.py
@@ -0,0 +1,10 @@
+# Ejercicio 3: Un programa que calcule y muestre el área de un cuadrado de lado igual a 5.
+# *Definimos el lado del cuadrado.
+
+lado = 5
+
+# *Calculamos el área (lado por lado).
+area = lado * lado
+
+# *Mostramos el resultado.
+print("El área del cuadrado es:", area)
