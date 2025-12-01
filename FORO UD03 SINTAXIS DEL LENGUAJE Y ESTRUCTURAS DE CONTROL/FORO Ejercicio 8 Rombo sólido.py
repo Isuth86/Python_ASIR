e49432e51f98342dea0c2740c73507033f0d6cdd
Isma86 <isberrio86@gmail.com>
2025-12-01 15:24:57 +0100

    *

--- FORO UD03 SINTAXIS DEL LENGUAJE Y ESTRUCTURAS DE CONTROL/FORO Ejercicio 8 Rombo sólido.py
+++ FORO UD03 SINTAXIS DEL LENGUAJE Y ESTRUCTURAS DE CONTROL/FORO Ejercicio 8 Rombo sólido.py
@@ -1,5 +1,6 @@
 # Ejercicio 8: Imprime un rombo sólido de altura 2n-1, centrado, usando asteriscos.
 # Figura para n=4:
+
 try:
     n = int(input("Introduce n (n >= 1): "))
     if n < 1:
@@ -7,10 +8,14 @@ try:
 except ValueError as e:
     print("Error:", e)
 else:
-    altura = 2 * n - 1
-    for i in range(1, altura + 1):
-        # distancia a la fila central
-        d = abs(n - i)
-        espacios = d
-        asteriscos = 2 * (n - d) - 1
+    # Parte superior (incluye la fila central)
+    for i in range(1, n + 1):
+        espacios = n - i
+        asteriscos = 2 * i - 1
+        print(" " * espacios + "*" * asteriscos)
+
+    # Parte inferior
+    for i in range(n - 1, 0, -1):
+        espacios = n - i
+        asteriscos = 2 * i - 1
         print(" " * espacios + "*" * asteriscos)
