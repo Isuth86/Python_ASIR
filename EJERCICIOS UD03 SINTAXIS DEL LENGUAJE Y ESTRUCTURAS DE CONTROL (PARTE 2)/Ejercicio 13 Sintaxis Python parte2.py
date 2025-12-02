a599a54917caace2dacd415e645205787c87a2a6
Isma86 <isberrio86@gmail.com>
2025-12-02 15:55:30 +0100

    *

--- EJERCICIOS UD03 SINTAXIS DEL LENGUAJE Y ESTRUCTURAS DE CONTROL (PARTE 2)/Ejercicio 13 Sintaxis Python parte2.py
+++ EJERCICIOS UD03 SINTAXIS DEL LENGUAJE Y ESTRUCTURAS DE CONTROL (PARTE 2)/Ejercicio 13 Sintaxis Python parte2.py
@@ -1,14 +1,10 @@
 # * Ejercicio 13
 # Crea una aplicación que dibuje una escalero de números, siendo cada línea números empezando
-try:
-    altura = int(input("Introduce la altura: "))
-    if altura <= 0:
-        raise ValueError
-except ValueError:
-    print("ERROR: Debes introducir un número mayor que cero")
-else:
-    # Construimos la escalera de números.
-    for i in range(1, altura + 1):
-        for j in range(1, i + 1):
-            print(j, end="")
-        print()
+# Escalera de números sin control de errores
+
+altura = int(input("Introduce la altura: "))
+
+for i in range(1, altura + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
